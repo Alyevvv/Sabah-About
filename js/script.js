f66eee077f23bf304ac2d6c662f10f4e6c8b12fa
ZHalaMustafayeva <68350142+ZHalaMustafayeva@users.noreply.github.com>
2020-10-14 11:08:56 +0400

    all

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // TESTIMOONIALS SECTION
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:3,
-        itemsDesktop:[1000,3],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,2],
-        itemsMobile:[650,1],
-        pagination:true,
-        autoPlay:true
-    });
+// $(document).ready(function(){
+//     $(".events-section").owlCarousel({
+//         items:3,
+//         itemsDesktop:[1000,3],
+//         itemsDesktopSmall:[979,2],
+//         itemsTablet:[768,2],
+//         itemsMobile:[650,1],
+//         pagination:true,
+//         autoPlay:true
+//     });
+// });
+
+$('.events-section').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+
 });
