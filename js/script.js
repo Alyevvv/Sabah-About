e9833ee3fd84202807ef566de13ec3e029d77d3c
ZHalaMustafayeva <68350142+ZHalaMustafayeva@users.noreply.github.com>
2020-10-23 15:19:12 +0400

    correction

--- js/script.js
+++ js/script.js
@@ -19,3 +19,24 @@ window.onload = function() {
     preloader.style.display = 'none';
 };
 
+$('.announcement-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    prevArrow: $('.prev-arrow-announcement'),
+    nextArrow: $('.next-arrow-announcement')
+});
+
+$('.events').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    prevArrow: $('.prev-arrow-events'),
+    nextArrow: $('.next-arrow-events')
+});
